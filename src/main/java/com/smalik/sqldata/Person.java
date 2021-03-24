2f5547b2444ad00e9ab1b551a9ee05ba8b2870d0
pkalapala <63861560+pkalapala@users.noreply.github.com>
2021-03-24 14:37:25 -0400

    Added emoji to Person

--- src/main/java/com/smalik/sqldata/Person.java
+++ src/main/java/com/smalik/sqldata/Person.java
@@ -1,6 +1,7 @@
 package com.smalik.sqldata;
 
 import javax.persistence.*;
+import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -29,4 +30,6 @@ public class Person {
 	@NotNull
     @Min(5)	
 	private int age;
+		
+	private String emoji;
 }
